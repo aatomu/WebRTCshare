39733f794cdb2063cdee741b8a18323323450746
aatomu <aatomu21263+git@gmail.com>
2024-09-16 19:13:34 +0900

    変数登録忘れ

--- src/assets/pull.js
+++ src/assets/pull.js
@@ -1,52 +1,54 @@
 async function newPullSession(sourceID) {
-      // "Create a New Session" request
-      remoteStatus.innerText = "Create session"
-      const sessionID = await newSession()
-      // Create "local WevRTC" connection
-      remoteStatus.innerText = "Connecting session"
-      const connection = await createPeerConnection()
-      // Pull tracks request
-      remoteStatus.innerText = "Sending pull tracks"
-      const pullResponse = await pullTrack(sessionID, sourceID)
-      // Track resolve check
-      const resolveTracks = Promise.all(
-        pullResponse.tracks.map(({ mid }) =>
-          new Promise((resolve, reject) => {
-            setTimeout(reject, 5000)
-            function checkTrack({ transceiver, track }) {
-              if (transceiver.mid !== mid) return
-              connection.removeEventListener("track", checkTrack)
-              resolve(track)
-            }
-            connection.addEventListener("track", checkTrack)
-          })
-        )
-      )
-      // Renegotiation WebRTC
-      if (pullResponse.requiresImmediateRenegotiation) {
-        remoteStatus.innerText = "Renegotiation connection"
-        await connection.setRemoteDescription(pullResponse.sessionDescription)
-        // Create an answer
-        const remoteAnswer = await connection.createAnswer()
-        // And set it as local description
-        await connection.setLocalDescription(remoteAnswer)
-        // Send our answer back to the Calls API
-        remoteStatus.innerText = "Sending renegotiation config"
-        const renegotiateResponse = await renegotiateSession(sessionID, remoteAnswer)
-        if (renegotiateResponse.errorCode) {
-          remoteStatus.innerText = "Error: failed send renegotiation config"
-          throw new Error(renegotiateResponse.errorDescription)
+  const stat = document.getElementById("stat")
+  // "Create a New Session" request
+  stat.innerText = "Create session"
+  const sessionID = await newSession()
+  // Create "local WevRTC" connection
+  stat.innerText = "Connecting session"
+  const connection = await createPeerConnection()
+  // Pull tracks request
+  stat.innerText = "Sending pull tracks"
+  const pullResponse = await pullTrack(sessionID, sourceID)
+  // Track resolve check
+  const resolveTracks = Promise.all(
+    pullResponse.tracks.map(({ mid }) =>
+      new Promise((resolve, reject) => {
+        setTimeout(reject, 5000)
+        function checkTrack({ transceiver, track }) {
+          if (transceiver.mid !== mid) return
+          connection.removeEventListener("track", checkTrack)
+          resolve(track)
         }
-      }
-      // Wait pull Tracks
-      remoteStatus.innerText = "Wait pull tracks"
-      const pulledTracks = await resolveTracks;
-      // Remote Preview
-      remoteStatus.innerText = "Create remote preview"
-      const stream = new MediaStream();
-      document.getElementById("remote").srcObject = stream
-      document.getElementById("remote").play()
-      pulledTracks.forEach((t) => stream.addTrack(t));
-  
-      remoteStatus.innerText = "Complete!"
+        connection.addEventListener("track", checkTrack)
+      })
+    )
+  )
+  // Renegotiation WebRTC
+  if (pullResponse.requiresImmediateRenegotiation) {
+    stat.innerText = "Renegotiation connection"
+    await connection.setRemoteDescription(pullResponse.sessionDescription)
+    // Create an answer
+    const remoteAnswer = await connection.createAnswer()
+    // And set it as local description
+    await connection.setLocalDescription(remoteAnswer)
+    // Send our answer back to the Calls API
+    stat.innerText = "Sending renegotiation config"
+    const renegotiateResponse = await renegotiateSession(sessionID, remoteAnswer)
+    if (renegotiateResponse.errorCode) {
+      stat.innerText = "Error: failed send renegotiation config"
+      throw new Error(renegotiateResponse.errorDescription)
+    }
+  }
+  // Wait pull Tracks
+  stat.innerText = "Wait pull tracks"
+  const pulledTracks = await resolveTracks;
+  // Remote Preview
+  stat.innerText = "Create remote preview"
+  const stream = new MediaStream();
+  const preview = document.getElementById("preview")
+  preview.srcObject = stream
+  preview.play()
+  pulledTracks.forEach((t) => stream.addTrack(t));
+
+  stat.innerText = "Complete!"
 }
