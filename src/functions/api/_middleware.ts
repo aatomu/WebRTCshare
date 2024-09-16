e8f71d5d22c31db60ec43970cce6c638e4050d68
aatomu <aatomu21263+git@gmail.com>
2024-09-16 16:25:19 +0900

    test

--- src/functions/api/_middleware.ts
+++ src/functions/api/_middleware.ts
@@ -1,13 +1,13 @@
 interface Env {}
 
 //export async function onRequestGet(context:EventContext): Promise<Response> {
-export async function onRequestGet(context): Promise<Response> {
+export async function onRequestGet({context,env}): Promise<Response> {
   const request: Request = context.request;
 
   const REQUEST_PATH = new URL(request.url).pathname.split("/");
   //const REQUEST_EntryPoint = REQUEST_PATH[1];
   const REQUEST_SITE = REQUEST_PATH[2];
   const REQUEST_API = REQUEST_PATH[3];
-
+  console.log(env,REQUEST_PATH)
   return new Response("404 Not Found");
 }
