a636dbe3a05fafa59e2d64e6ab23b155df7599dc
aatomu <aatomu21263+git@gmail.com>
2024-09-16 16:43:13 +0900

    修正

--- src/functions/api/_middleware.ts
+++ src/functions/api/_middleware.ts
@@ -5,13 +5,12 @@ interface Env {
   turnToken: string;
 }
 
-export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    const REQUEST_PATH = new URL(request.url).pathname.split("/");
-    //const REQUEST_EntryPoint = REQUEST_PATH[1];
-    const REQUEST_SITE = REQUEST_PATH[2];
-    const REQUEST_API = REQUEST_PATH[3];
-    console.log(env, REQUEST_PATH);
-    return new Response("404 Not Found");
-  },
+export const onRequest: PagesFunction<Env> = async (context) => {
+  const request: Request = context.request;
+  const REQUEST_PATH = new URL(request.url).pathname.split("/");
+  //const REQUEST_EntryPoint = REQUEST_PATH[1];
+  const REQUEST_SITE = REQUEST_PATH[2];
+  const REQUEST_API = REQUEST_PATH[3];
+  console.log(context.env, REQUEST_PATH);
+  return new Response("404 Not Found");
 };
