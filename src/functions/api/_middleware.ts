73a996ed5bf318e7c5ac7081bb776739e179c6de
aatomu <aatomu21263+git@gmail.com>
2024-09-16 16:35:49 +0900

    envテスト

--- src/functions/api/_middleware.ts
+++ src/functions/api/_middleware.ts
@@ -1,13 +1,17 @@
-interface Env {}
-
-//export async function onRequestGet(context:EventContext): Promise<Response> {
-export async function onRequestGet({context,env}): Promise<Response> {
-  const request: Request = context.request;
-
-  const REQUEST_PATH = new URL(request.url).pathname.split("/");
-  //const REQUEST_EntryPoint = REQUEST_PATH[1];
-  const REQUEST_SITE = REQUEST_PATH[2];
-  const REQUEST_API = REQUEST_PATH[3];
-  console.log(env,REQUEST_PATH)
-  return new Response("404 Not Found");
+interface Env {
+  appID: string;
+  token: string;
+  turnID: string;
+  turnToken: string;
 }
+
+export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    const REQUEST_PATH = new URL(request.url).pathname.split("/");
+    //const REQUEST_EntryPoint = REQUEST_PATH[1];
+    const REQUEST_SITE = REQUEST_PATH[2];
+    const REQUEST_API = REQUEST_PATH[3];
+    console.log(env, REQUEST_PATH);
+    return new Response("404 Not Found");
+  },
+};
